2b048ebc6e94c73a96f3f9d70d419dc9c222c321
Lucas Demasi <ldemasi@student.saintaug.NSW.edu.au>
2024-04-01 15:33:32 +1100

    added a play again prompt, still need to fix update categories

--- Connection Assessment task.py
+++ Connection Assessment task.py
@@ -1,5 +1,16 @@
 import random
 
+def play_again_prompt():
+    play_again_prompt = input("would you like another chance Type: Y for Yes or N for No: ")
+    if play_again_prompt == "Y" or "y":
+        main()
+        return True
+    elif play_again_prompt == "N" or "n":
+        return True
+    elif play_again_prompt not in ("Y" or "y", "N" or "n"):
+        print("please type in")
+        return False
+    
 def get_user_guesses():
     # get 4 inputs 
     # these inputs must be words
@@ -8,9 +19,11 @@ def get_user_guesses():
     
     guesses = []
     for i in range(0,4):
-        guess = input() 
-        guesses.append(guess)
-
+        try:
+            guess = input("Guess {}: ".format(i+1))
+            guesses.append(guess)
+        except Exception as e:
+            print("invalid input: ", e)
     return guesses
 
 def check_guess(guesses,word_categories):
@@ -28,13 +41,12 @@ def check_guess(guesses,word_categories):
         return True, category
     else:
         print("Incorrect!")
-        return False, None
+        return False, None      
 # need to make this check for, if three of the four words in the same catgory are correct
-# Then make another else: that says if you the user has pick only 1 or 2 of the words than they will get a message that say try again           
-
+# Then make another else: that says if you the user has pick only 1 or 2 of the words than they will get a message that say try again
 def print_words_from_categories(word_categories):
-    """
-    Loop through each category to print its words.
+    """ 
+    Loop through each category to print its words. 
     """
     for category in word_categories:
         for word in category['words']:
@@ -53,7 +65,7 @@ def setup_word_categories():
     }
     category2 = {
     "linking_word": "lazy",
-    "words": ["Sloth", "Couch", "Sleepy", "Indolent"]
+    "words": ["Sloth", "Loafer", "Sleepy", "Indolent"]
     }
     category3 = {
     "linking_word": "smelly",
@@ -68,20 +80,20 @@ def setup_word_categories():
     "words": ["Bird", "Curry", "James", "Jordan"]
     }
     category6 = {
-    "linking_word": "parts of a news paper",
-    "words": ["sports", "Opinion", "Entertainment", "Headline"]
+    "linking_word": "sections of a news paper",
+    "words": ["Sports", "Opinion", "Entertainment", "Headlines"]
     }
     category7 = {
-    "linking_word": "Iconic video game characters",
-    "words": ["COD black ops", "Minecraft", "Halo", "GoldenEye"]
+    "linking_word": "Iconic video games",
+    "words": ["COD Black Ops", "Minecraft", "Halo", "GoldenEye"]
     }
     category8 = {
-    "linking_word": "can / are deep fried",
-    "words": ["Arancini", "Egg roll", "Chicken", "Tofu"]
+    "linking_word": "Snack foods that can be deep fried",
+    "words": ["Chips", "Egg roll", "Chicken", "Tofu"]
     }
     category9 = {
-    "linking_word": "words for walking heavily",
-    "words": ["Stomping", "Rrudged", "Hiking", "Trek"]
+    "linking_word": "Verbs for walking heavily",
+    "words": ["Stomping", "Trudging", "Hiking", "Treking"]
     }
     
     word_categories.append(category1)
@@ -102,7 +114,6 @@ def setup_word_categories():
     "make sure it doesn't grab the same numbers twice"
     return selected_categories
 # Need to add more categories
-
 def create_empty_grid():
     """
     Create a 4x4 grid with a placeholder word in each cell.
@@ -167,13 +178,18 @@ def shuffle_words(grid):
     # print(grid)
     return grid
 
-def play_game():
-    pass
 
-def update_categories(selected_categories, guessed_categories):
-    for category in guessed_categories:
-        selected_categories.remove(category)
-        selected_categories.insert(0, category)
+def update_categories(selected_categories, guessed_category):
+    if category in guessed_category:
+        for category in selected_categories:
+            category.remove(guessed_category)
+            category.insert(0, guessed_category)
+    
+    # need to create an additional function that would rebuild the game grid from 
+    # two different sources, the categories that have been guessed correctly, and 
+    #then the categories that have not been guessed yet. I’ll leave it up to you
+    # to try work out how to do this.
+    
     # take the original 4 categories defined at the beginning of the game
     # take the guesssed category that was right
     # remove the guessed category from the lsit of 4 categories
@@ -183,16 +199,21 @@ def main():
     # This is the order in which things occur
     # need to loop the main so that the user can play to game more than once without haveing to relaunch the code
     word_categories = []
+    game_won = False
+    guessed_categories = 0
+    lives = 4
+    correct_words = []
+    correct_words.append('hello')
     selected_categories = setup_word_categories() # go and grab the 4 categories to use in the game
     grid = create_empty_grid() # creates a grid
     populated_grid = populate_grid(word_categories, grid) #populates the grid with the words from chosen categories
     unsorted_grid = populate_grid(selected_categories, grid) 
     shuffled_grid = shuffle_words(unsorted_grid) # shuffles the words in the grid 
     make_grid_look_nice(shuffled_grid)
-
     game_won = False
     guessed_categories = 0
     lives = 4
+    correct_words = []
 
     while lives > 0 and game_won == False:
         print(f"You have {lives} guesses remaining")
@@ -205,38 +226,21 @@ def main():
         else:
             guessed_categories
             print(f"You guessed correctly. The category is {category_guessed['linking_word']}")
-            update_categories(selected_categories, guessed_categories)
+            
 
 
-        if len(guessed_categories) == 4:
+        if guessed_categories == 4:
             game_won = True
             print("You win the game.")
+            play_again_prompt()
+
 
     if lives == 0:
         print("You ran out of guesses. Game over.")
-
-
-    # game_won = False
-    # guessed_categories = 0
-    # lives = 4
-    # while lives > 0 and game_won == False:
-    #     print(f"You have {lives} guesses remaining")
-    #     guesses = get_user_guesses()
-    #     guess_result, category_guessed = check_guess(guesses, selected_categories)
-    #     print(shuffled_grid)
-
-
-    #     if guess_result == False:
-    #         lives = lives - 1
-    #     else:
-    #         guessed_categories += 1
-    #         print(f"You guessed correctly. The category is {category_guessed}")
-    #     if guessed_categories == 4:
-    #         game_won = True
-    #         print("You win the game.")
-    
-
-
+        play_again_prompt()
 main()
+# Make a restart game fucntion
+# Fix the update categories function
+# make sure that the user cannot use the same words he has already found
 
-# Need to create a lives function the ticks down if they get one of the word wrong
+    
